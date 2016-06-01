57c048a9e37e648d8085ffb7138dd4d9a6be9ab9
Slaiiz <crocoshack@gmail.com>
2016-06-01 19:22:39 +0200

    Aesthetic modifications.

--- include/lemin.h
+++ include/lemin.h
@@ -6,7 +6,7 @@
 /*   By: vchesnea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/24 18:32:04 by vchesnea          #+#    #+#             */
-/*   Updated: 2016/05/31 18:27:31 by vchesnea         ###   ########.fr       */
+/*   Updated: 2016/06/01 13:33:17 by vchesnea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,22 @@
 
 # include "libft.h"
 
-typedef struct		s_item
+typedef enum		e_command
 {
-	t_command		cmd;
-	const char		*name;
-}					t_item;
+	COMMAND_NONE,
+	COMMAND_END,
+	COMMAND_START,
+	COMMAND_FIREPIT,
+	COMMAND_WORMHOLE,
+	COMMAND_DEATHSQUAD
+}					t_command;
 
 typedef struct		s_room
 {
 	unsigned int	x;
 	unsigned int	y;
+	const char		*name;
+	t_command		command;
 }					t_room;
 
 typedef struct		s_node
@@ -45,22 +51,13 @@ typedef struct		s_anthill
 typedef enum		e_error
 {
 	ERROR_OK,
+	ERROR_MALLOC,
 	ERROR_BAD_INPUT,
 	ERROR_MALFORMED_ROOM,
 	ERROR_MALFORMED_NODE,
 	ERROR_MALFORMED_COMMAND,
 	ERROR_MALFORMED_COMMENT,
 }					t_error;
-
-typedef enum		e_command
-{
-	COMMAND_NONE,
-	COMMAND_END,
-	COMMAND_START,
-	COMMAND_FIREPIT,
-	COMMAND_WORMHOLE,
-	COMMAND_DEATHSQUAD
-}					t_command;
 
 t_error				read_game_data(const char *s, t_anthill **out);
 unsigned int		key_to_hash(const char *s);
