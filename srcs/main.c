95844d87916e799b3901a434ce0312f4a8fde965
Slaiiz <crocoshack@gmail.com>
2016-06-03 19:12:11 +0200

    Still going.

--- srcs/main.c
+++ srcs/main.c
@@ -12,6 +12,60 @@
 
 #include "lemin.h"
 
+static int	build_room_data(t_list *in, t_anthill *in)
+{
+	size_t	len;
+	t_list	*tmp;
+
+	if ((len = ft_lstlen(in)) < 1)
+		return (1);
+	tmp = in;
+	while (tmp)
+}
+
+/*
+** process_input:
+** Step 1: Ant count check
+** Step 2: Room definition check
+** - Command
+** - Comment
+** - Room definition
+** Step 3: Node definition check
+** - Comment
+** - Node definition
+*/
+
+static int	parse_input(const char *s, t_anthill *in)
+{
+	t_list		*items;
+	t_command	command;
+
+	if (ft_expect(&s, "$nw\n", &in->n_ants))
+		return (1);
+	items = NULL;
+	while (*s != '\0')
+	{
+		if ((!ft_expect(&s, "##") && process_command(&s, &command)
+			&& process_room(&s, command, &items))
+			|| (!ft_expect(&s, "#") && process_comment(&s)))
+			return (1);
+		else if (process_room(&s, COMMAND_NONE, &items))
+			break ;
+	}
+	if (build_room_data(items, in))
+		return (1);
+	while (*s != '\0')
+		if ((!ft_expect(&s, "#") && process_comment(&s))
+			|| (process_node(&s, in)))
+			return (1);
+	return (0);
+}
+
+/*
+** read_input:
+** Read data from the standard input into a buffer.
+*/
+
 static int	read_input(const char **out)
 {
 	size_t		len;
@@ -40,12 +94,16 @@ static int	read_input(const char **out)
 	return (0);
 }
 
+/*
+** main:
+*/
+
 int			main(int argc, char **argv)
 {
+	t_anthill	data;
 	const char	*input;
-	t_anthill	*gamedata;
 
-	if (read_input(&input) || read_game_data(input, &gamedata) != ERROR_OK)
+	if (read_input(&input) || process_input(input, &data))
 	{
 		ft_printf("#!fd=2^ERROR\n");
 		return (ERROR_BAD_INPUT);
